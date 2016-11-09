80e548a90333b0c4d91481be6c97bf7a60d6e113
xionglei <342256048@qq.com>
2016-11-09 18:42:35 +0800

    modify client.py and add node type

--- src/client.py
+++ src/client.py
@@ -4,6 +4,14 @@ GLOBAL_PROXY_PORT = 8080
 PROXY_SERVER_IP = '23.83.226.130'
 PROXY_SERVER_PORT = 8080
 
+NODE_TYPE_NORMAL_CLIENT = 0
+NODE_TYPE_NORMAL_SERVER = 1
+NODE_TYPE_PROXY_CLIENT = 2
+NODE_TYPE_PROXY_SERVER = 3
+
+#全局node列表
+node = {}
+
 #流量类，分为上行流量和下行流量
 class flow():
 	def __init__(self):
@@ -25,17 +33,35 @@ class node():
 		#设置节点收发缓冲区
 		self.snd_buf = None
 		self.rcv_buf = None
+        
+        self.node_type = -1
 
 	def handle_flow(self):
-		if self.flow.is_down_flow:
+        #没有数据
+        if self.flow is None:
+            pass
+            
+        #如果是普通的客户端节点
+		if self.node_type is NODE_TYPE_NORMAL_CLIENT:
 			if self.downward_fd == -1:
 				self.sockfd.send(self.flow.data)
 			else:
 				write(self.downward_fd, self.flow.data)
-		else:
+		
+        #如果是普通的服务端节点
+        if self.node_type is NODE_TYPE_NORMAL_SERVER:
 			if self.upward_fd == -1:
 				self.sockfd.recv(self.flow.data)
 			else:
+            
+        #如果是代理节点客户端
+        if self.node_type is NODE_TYPE_PROXY_CLIENT:
+            pass
+            
+        
+        #如果是代理节点服务端
+        if self.node_type is NODE_TYPE_PROXY_SERVER:
+            pass
 				
 
 
@@ -54,7 +80,7 @@ def main_loop():
 	epoll.register(ssock.fileno(), select.EPOLLIN)
 
 	try:
-	   node = {}
+	   
 	   while True:
 	      events = epoll.poll(1)
 	      for fileno, event in events:
