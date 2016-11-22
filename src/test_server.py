ef6633d1711456680703fcac46853ae48f0cba2f
xionglei <342256048@qq.com>
2016-11-22 18:41:04 +0800

    chagne test code

--- src/test_server.py
+++ src/test_server.py
@@ -5,14 +5,14 @@ import socket
 tcpCliSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 tcpCliSock.bind(('127.0.0.1', HTTPS_PROT))
-tcpCliSock.listen(1)
+tcpCliSock.listen(5)
 
 while True:
-	ss, addr = tcpCliSock.accept()
-	try:
-		while True:
-			str = ss.recv(1024)
-			print str
-			ss.send("Hello world message comes from loopback server.!")
-	finally:
-		ss.close()
+    try:
+        ss, addr = tcpCliSock.accept()
+        str = ss.recv(1024)
+        print str
+        ss.send("Hello world message comes from loopback server.!")
+        ss.close()
+    except:
+        pass
